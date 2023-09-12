eb8b4d504ffe19f9c9a6361aa0cbf9873e03f337
ameer31 <ameer.hamza7433@gmail.com>
2023-09-12 17:41:38 +0500

    last 2 question remaining

---
+++ app.js
@@ -0,0 +1,87 @@
+
+//for loop
+// Q 3 solved
+
+// var user = +prompt('Enter the table number that you want to print.')
+// var print =prompt('yaha tk table print krdo')
+
+// for(var i = 1; i<=print; i++){
+//     document.write(user+' x '+i+ ' = '+ user*i+'<br>')}
+
+//for loop
+// Q 5 solved
+
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for (var i = 0; i < fruits.length; i++) {
+//   document.write(fruits[i]+'<br>');
+// }
+
+//for loop
+// Q 8 solved
+//  var user = prompt('enter the item')
+//  var a =['apple','orange','banana','mango']
+// var match =false
+
+//  for(var i = 0; i<a.length; i++){
+//     if(user == a[i]){
+//         match = true
+//         alert('yes yeh mil jahy ga')
+//         break
+//     }
+//  }
+// if(match ==false){
+//     alert('asi koi chez nh humary pass')
+// }
+
+
+
+//for loop
+// Q 13 solved
+
+// var std = prompt('enter your name')
+// var scr = +prompt('enter your scores')
+
+// document.write(`<tbody><table border =1><tr> <th> student name</th>  <td> ${std}</td> </tr>
+
+// <tr> <th> scores </th>  <td>${scr} </td></tr> </table></tbody>`)
+
+//for loop
+// Q 15 solved
+
+// var A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+// for ( var i =0; i<A.length; i++){
+//     for (var j = 0; j<A[i].length; j++ ){
+//         document.write(A[i][j])
+        
+//     }
+//     document.write('<br>')
+// }
+
+
+// function 
+// Q  3 solved
+
+// function nam(firstname,lastname){
+//     return firstname+lastname
+// }
+// var a = prompt('enter fisrtname')
+// var b = prompt('enter second value')
+
+// alert(nam(a,b))
+
+// function 
+// Q  5 solved
+
+// function sum(a,b){
+//     return a+b
+// }
+// var a = +prompt('enter your value')
+// var b = +prompt('enter your value')
+
+// document.write(sum(a,b))
+
+// function 
+// Q  19
